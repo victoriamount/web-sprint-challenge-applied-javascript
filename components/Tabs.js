37c54aef09c180e2f8747b75218d68ee93cf52c9
Victoria Mount <victoriamount01@gmail.com>
2020-10-02 10:31:41 -0500

    Complete Tabs section of MVP

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,23 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+const topicsSection = document.querySelector('.topics')
+
+axios.get('https://lambda-times-api.herokuapp.com/topics')
+    .then(res => {
+        for (i = 0; i < res.data.topics.length; i++){
+            topicsSection.appendChild(tabConstructor(res.data.topics[i]))
+        }
+    })
+    .catch(err => {
+        debugger;
+    })
+
+function tabConstructor(topic){
+    const tab = document.createElement('div')
+    tab.classList.add('tab')
+    tab.textContent = topic
+
+    return tab // Wasted time here, DON'T FORGET IN THE FUTURE
+}
